366160a76d5514b025d7e0f81b1108bcd4b9609f
Shohei SUNAGAWA <kanpe777@gmail.com>
2013-09-06 06:36:06 +0000

    Copy search -> search_support

--- app/controllers/search_support_controller.rb
+++ app/controllers/search_support_controller.rb
@@ -1,4 +1,27 @@
 class SearchSupportController < ApplicationController
   def index
+    render 'search_support/index'
+  end
+
+  def support
+    keyword = params['keyword']
+    httpClient = HTTPClient.new
+
+    @jsonData = nil
+    @errorMeg = nil
+
+    begin
+      data = httpClient.get_content('https://app.rakuten.co.jp/services/api/IchibaItem/Search/20130805', {
+          'applicationId' => '1046125781907581411',
+          'affiliateId'   => '11b23db1.acb56b0a.11b23db2.0c5a2bef',
+          'keyword'       => keyword
+      })
+      @jsonData = JSON.parse data
+      p @jsonData
+    rescue HTTPClient::BadResponseError => e
+    rescue HTTPClient::TimeoutError => e
+    end
+
+    render 'search_support/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ RakutenSample::Application.routes.draw do
   get "search_support/index"
   root 'itemsearch#index'
   post '/search' => 'itemsearch#search'
+  post '/search_support' => 'search_support#support'
 end
