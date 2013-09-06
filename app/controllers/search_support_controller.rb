ac0f6e8a89726b72893ac6b8ae89571b274ed32a
Shohei SUNAGAWA <kanpe777@gmail.com>
2013-09-06 08:30:44 +0000

    Add Function Searchsupport

--- app/controllers/search_support_controller.rb
+++ app/controllers/search_support_controller.rb
@@ -8,6 +8,7 @@ class SearchSupportController < ApplicationController
     httpClient = HTTPClient.new
 
     @jsonData = nil
+    @cateData = nil
     @errorMeg = nil
 
     begin
@@ -17,9 +18,25 @@ class SearchSupportController < ApplicationController
           'keyword'       => keyword
       })
       @jsonData = JSON.parse data
-      p @jsonData
-    rescue HTTPClient::BadResponseError => e
-    rescue HTTPClient::TimeoutError => e
+      #p @jsonData
+      @cateData = Hash.new
+      @jsonData['Items'].each do |element| 
+        key = element['Item']['genreId']
+=begin
+        genreInfo = JSON.parse httpClient.get_content('https://app.rakuten.co.jp/services/api/IchibaGenre/Search/20130723', {
+          'applicationId' => '1046125781907581411',
+          'affiliateId'   => '11b23db1.acb56b0a.11b23db2.0c5a2bef',
+          'genreId'       => key
+         })
+        puts "keyInfo は #{keyInfo}"
+        #genreKey = genreInfo['current']['genreName']
+=end
+        genreKey = key
+        @cateData[genreKey] = (@cateData[genreKey].nil?)? 1 : @cateData[genreKey] + 1
+      end
+
+      rescue HTTPClient::BadResponseError => e
+      rescue HTTPClient::TimeoutError => e
     end
 
     render 'search_support/index'
